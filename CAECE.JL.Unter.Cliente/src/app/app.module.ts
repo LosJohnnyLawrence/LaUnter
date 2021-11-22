a311381e94436851313d28ce871e544e2eac009b
jhonnylawrence <jhonnylawrence@jhonnylawrence>
2021-11-22 17:56:22 -0300

    Miracion a matrtial 1

--- CAECE.JL.Unter.Cliente/src/app/app.module.ts
+++ CAECE.JL.Unter.Cliente/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MozoComponent } from './componentes/mozo/mozo.component';
@@ -20,6 +19,19 @@ import { DetalleSeleccionComponent } from './componentes/comun/detalle-seleccion
 import { DetalleItemComponent } from './componentes/comun/detalle-item/detalle-item.component';
 import { DetalleMesaComponent } from './componentes/comun/detalle-mesa/detalle-mesa.component';
 import { DetallePedidoComponent } from './componentes/comun/detalle-pedido/detalle-pedido.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatExpansionModule} from '@angular/material/expansion';
+  
 
 @NgModule({
   declarations: [
@@ -40,11 +52,22 @@ import { DetallePedidoComponent } from './componentes/comun/detalle-pedido/detal
     DetalleSeleccionComponent,
     DetalleItemComponent,
     DetalleMesaComponent,
-    DetallePedidoComponent
+    DetallePedidoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,  
+    MatExpansionModule,  
+    FlexLayoutModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CAECE.JL.Unter.Cliente/src/app/componentes/nav-bar/nav-bar.component.ts
+++ CAECE.JL.Unter.Cliente/src/app/componentes/nav-bar/nav-bar.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  
+ rutas = [
+    { nombre: 'Mozo', url: '/mozo' },
+    { nombre: 'Caja', url: '/caja' },
+    { nombre: 'Cocina', url: '/cocina' },
+    { nombre: 'Config', url: '/config' },
+    { nombre: 'Ayuda', url: '/ayuda' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
